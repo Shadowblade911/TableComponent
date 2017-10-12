9c9dc278f814a2815ee58e52da5191cac492a654
Shadowblade911 <david.pogin@centriam.com>
2017-10-12 14:25:01 -0500

    Fixing paging for back end (#15)
    * having pages use viewPage instead of currentPage
    
    * having pages use viewPage instead of currentPage
    
    * rebased work onto master

--- src/centriamTable/CentriamTable.js
+++ src/centriamTable/CentriamTable.js
@@ -299,7 +299,7 @@ export default class CentriamTable extends React.Component {
                         </div>
                         <div>
                             <button className="defined"
-                                    disabled={this.state.currentPage === 1}
+                                    disabled={this.state.viewPage === 1}
                                     onClick={()=>{
                                         this.state.changePageFunction(this.state.currentPage-1);
                                     }}
@@ -307,7 +307,7 @@ export default class CentriamTable extends React.Component {
                                 <span className="previous"></span>
                             </button>
                             <button className="defined"
-                                    disabled={this.state.currentPage === this.state.maxPage}
+                                    disabled={this.state.viewPage === this.state.maxPage}
                                     onClick={()=>{
                                         this.state.changePageFunction(this.state.currentPage+1);
                                     }}
